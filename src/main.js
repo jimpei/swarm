9bc97d64a03983a119cadfc485b15322aa398f0f
jimpei <j.hamamura0621@gmail.com>
2019-09-07 15:46:21 +0900

    add firebase.js

--- src/main.js
+++ src/main.js
@@ -2,22 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import firebase from "firebase";
+import Firebase from "./firebase";
+Firebase.init();
 
 Vue.config.productionTip = false;
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAv6tanUdTFNFevBBrlDLcgpUz9tD23Cg4",
-  authDomain: "swarm-16280.firebaseapp.com",
-  databaseURL: "https://swarm-16280.firebaseio.com",
-  projectId: "swarm-16280",
-  storageBucket: "swarm-16280.appspot.com",
-  messagingSenderId: "196507500794",
-  appId: "1:196507500794:web:495a22fce344b6c1"
-};
-
-firebase.initializeApp(firebaseConfig);
-// export default firebase;
 
 new Vue({
   router,
