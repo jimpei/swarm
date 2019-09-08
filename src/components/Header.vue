6e3542d7dc79b868f36cd018751ab10ea6f1cc56
jimpei <j.hamamura0621@gmail.com>
2019-09-08 18:04:37 +0900

    ドロップダウンメニュー作成

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,20 +8,18 @@
         <div v-if="user.photoURL"><img class="mr-3" :src="user.photoURL" width="40px"></div>
         <div v-else><img class="mr-3" src="../assets/fantasy_game_character_slime.png" width="40px"></div>
       </div>
-
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+      <div v-if="user.email">
+        <div class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          </a>
+          <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-left" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
         </div>
-      </li>
-
-
+      </div>
     </nav>
   </div>
 </template>
