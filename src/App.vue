a385601086218f6d38a55e711ca7c388f033d4d6
jimpei <j.hamamura0621@gmail.com>
2019-09-08 17:15:41 +0900

    bootstrap js の読み込み

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,6 @@
 
 <style>
 @import "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
-@import "https://code.jquery.com/jquery-3.3.1.slim.min.js";
-@import "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js";
-@import "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js";
 
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,6 +9,19 @@
         <div v-else><img class="mr-3" src="../assets/fantasy_game_character_slime.png" width="40px"></div>
       </div>
 
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+
+
     </nav>
   </div>
 </template>
